a43d40599876b70c64d44bafcbfa263bbb905151
Iker Zamorano <Iker Zamorano@HZ321161.H012108.NET>
2023-01-10 08:13:56 +0100

    compareTo

--- src/Articulos/Cerveza.java
+++ src/Articulos/Cerveza.java
@@ -86,4 +86,17 @@ public class Cerveza extends Articulo implements Alcohol {
 		return tasa;
 	}
 
+	@Override
+	public int compareTo(Articulo o) {
+		if (this.getPrecio() < o.getPrecio()) {
+			return -1;
+		} else {
+			if (this.getPrecio() > o.getPrecio()) {
+				return 1;
+			} else {
+				return 0;
+			}
+		}
+	}
+
 }
